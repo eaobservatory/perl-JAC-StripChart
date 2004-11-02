ee029e1026fd49e0e9306fd34150362e9bd1a13b
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-11-02 01:42:42 +0000

    Add init method. Cache frame object and plot object. Replot when limits change.
    svn path=/trunk/perlmods/; revision=7900

--- lib/JAC/StripChart/Sink/AST.pm
+++ lib/JAC/StripChart/Sink/AST.pm
@@ -41,9 +41,70 @@ $VERSION = sprintf("%d.%03d", q$Revision$ =~ /(\d+)\.(\d+)/);
 
 Methods are inherited from C<JAC::StripChart::Sink>.
 
+=head2 Class Specific Attributes
+
+=over 4
+
+=item B<astFrame>
+
+AST frame object associated with this plot.
+
+  $fr = $snk->astFrame;
+
+=cut
+
+sub astFrame {
+  my $self = shift;
+  if (@_) { $self->{AST_Frame} = shift; }
+  return $self->{AST_Frame};
+}
+
+=item B<astPlot>
+
+AST Plot object associated with this sink
+
+  $plt = $snk->astPlot;
+
+=cut
+
+sub astPlot {
+  my $self = shift;
+  if (@_) { $self->{AST_Plot} = shift; }
+  return $self->{AST_Plot};
+}
+
+=back
+
 =head2 General Methods
 
 =over 4
+
+=item B<init>
+
+Initialise the AST plotting system for this particular chart.
+
+  $snk->init( %attrs );
+
+Expects to receive the chart attributes for all the monitors serviced
+by this sink, as a hash with keys corresponding to the monitor label
+and values as Chart::Attrs objects.
+
+Probably need the chart title supplied here.
+
+=cut
+
+sub init {
+  my $self = shift;
+
+  # Create the AST plot and set the plotting attributes
+  my $fr = new Starlink::AST::Frame( 2, "title=Title,label(1)=time,unit(2)=MJD,label(2)=flux,unit(2)=Jy" );
+
+  $self->astFrame( $fr );
+
+  # We can not set plot attributes here since we are in principal creating
+  # a new plot each time the scale changes
+
+}
 
 =item B<putData>
 
@@ -53,11 +114,15 @@ Plot the data on the registered device using AST as the plotting engine.
 
 =cut
 
+my %cache;
 sub putData {
   my $self = shift;
   my ($chartid, $monid, $attr, @data ) = @_;
 
   # First need to store the new data in the cache indexed by monid
+  for my $elem (@data) {
+    $cache{$monid}{$elem->[0]} = $elem->[1];
+  }
 
   # then need to calculate statistics from the data for autoscaling
   my @x = map { $_->[0] } @data;
@@ -68,21 +133,64 @@ sub putData {
   my $ymax = max( @y );
   my $ymin = min( @y );
 
+  # Now we need to window this range to make sure things are as specified
+  # We should do all calculations on the cache rather than on the
+  # current data
+
   # Select the correct subsection
   $self->device()->select;
 
-  # now need to window the data
+  # Now need to find out whether the data range for plotting
+  # has changed. If it has we need to clear and recreate the
+  # plot.
+  my $isold = 0;
+  my $plt = $self->astPlot;
 
-  # Create the AST plot and set the plotting attributes
-  my $fr = new Starlink::AST::Frame( 2, "title=$chartid,label(1)=time,unit(2)=MJD,label(2)=flux,unit(2)=Jy" );
+  if (defined $plt) {
+    $isold = 1;
+    # We have a plot but we are not sure whether the bounds are okay
+    # Get the current plot bounds from the plot frame
+    my @bounds = $plt->PBox;
 
-  my $plt = new Starlink::AST::Plot( $fr, [0,0,1,1], [$xmin,$ymin,$xmax,$ymax], "" );
+    # see if the data bounds are inside these bounds
+    if ( $xmin < $bounds[0] ||
+	 $xmax > $bounds[1] ||
+	 $ymin < $bounds[2] ||
+	 $ymax > $bounds[3] ) {
+      # Clear the plot
+      $isold = 0;
+      $self->device->clear();
+      undef $plt;
+
+      # But make sure that we retrieve all the cache for replotting
+      # from the specified x-range
+      # Currently extract all
+      @x = ();
+      @y = ();
+      # Really need to replot all cached data for this plot
+      for my $x ( sort { $a <=> $b } keys %{ $cache{$monid} } ) {
+	push(@x, $x);
+	push(@y, $cache{$monid}->{$x});
+      }
+    }
+  }
+
+  # Create the new AST plot object if we do not have one
+  if (!defined $plt) {
+    $plt = new Starlink::AST::Plot( $self->astFrame(), [0,0,1,1],
+				    [$xmin,$ymin,$xmax,$ymax], "" );
+    $self->astPlot( $plt );
+  }
+
 
   # Register the correct plotting engine callbacks
   $self->_grfselect( $plt );
 
-  # draw the plot axes
-  $plt->Grid();
+  # draw the plot axes if we have changed the plot bounds
+  $plt->Grid() unless $isold;
+
+#  my $chan = new Starlink::AST::Channel( sink => sub { print "$_[0]\n"; } );
+#  $chan->Write( $plt );
 
   # plot the data using the requested attributes
   $plt->PolyCurve(\@x, \@y);
